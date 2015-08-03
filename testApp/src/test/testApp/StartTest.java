93ddc31c3d4e0ea3ea1b66bfc91e9362b06b5dc9
glorund <Roman.Cherednichenko@gmail.com>
2015-08-03 12:29:25 +0300

    change structure

---
+++ testApp/src/test/testApp/StartTest.java
@@ -0,0 +1,14 @@
+package testApp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StartTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
